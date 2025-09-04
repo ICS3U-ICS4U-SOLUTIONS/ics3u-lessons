5f6eada8e9b9e6b42ea9340d9849878a160ae965
e28166 <e28166@10.20.1.185>
2025-09-04 09:23:27 -0400

    FurryBunny

---
+++ src/lessons/HelloWorld.java
@@ -0,0 +1,18 @@
+package lessons;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		// this is a comment
+		// this is also a comment
+		
+		/* this is a
+		 * multi-line comment
+		 */
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
