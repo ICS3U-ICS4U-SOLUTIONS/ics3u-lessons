c31964510ceae697c0ba10594f3f2c5893d6afa5
e28166 <e28166@10.20.1.185>
2025-09-05 09:25:47 -0400

    s

---
+++ src/lessons/Variables.java
@@ -0,0 +1,20 @@
+package lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		// variables
+		int firstNum = 7;
+		int secondNum = 13;
+		int sum = 0;
+		
+		// processing
+		sum = firstNum + secondNum;
+		
+		// output
+		System.out.println(sum);
+
+	}
+
+}
