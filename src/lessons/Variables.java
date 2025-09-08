13709e054fef83430bfdbe89a71d94864af2fb3e
e28166 <e28166@10.20.1.185>
2025-09-08 08:32:15 -0400

    furry rabbit

---
+++ src/lessons/Variables.java
@@ -0,0 +1,26 @@
+package lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		// this is a comment
+		
+		/* this is a mutli-line
+		 * comment
+		 */
+		
+		// variables
+		int myInteger = 0;
+		double myDouble = 0.0;
+		char myChar = 'x';
+		String myString = "fubar foo bar";
+		boolean myBoolean1 = true;
+		boolean myBoolean2 = false;
+		
+		// constant variable
+		final double GST_RATE = 0.13; 
+		
+	}
+
+}
