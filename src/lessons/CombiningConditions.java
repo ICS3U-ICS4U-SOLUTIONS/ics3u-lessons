7d20e49bf70ee3dd13ddd79e931ee1b92fc3e2f5
e28166 <e28166@10.20.1.185>
2025-09-09 09:24:24 -0400

    s

--- src/lessons/CombiningConditions.java
+++ src/lessons/CombiningConditions.java
@@ -4,8 +4,19 @@ public class CombiningConditions {
 
 	public static void main(String[] args) {
 
-
+		// We can have more than 1 condition per statement.
+		// Keywords && (and) and || (or) can be used.
 		
+		// && (and) - both conditions have to be true
+		if ( (13 > 7) && (19 < 4) )  {
+			
+			System.out.println("true");
+		}
+		
+		else  {
+			
+			System.out.println("false");
+		}
 		
 		
 	}
