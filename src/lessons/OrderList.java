d0da4a0acdfcbc3a41c3bdc17dea7db3b6418279
e28166 <e28166@DESKTOP-56C86U1>
2025-10-02 08:51:26 -0400

    s

--- src/lessons/OrderList.java
+++ src/lessons/OrderList.java
@@ -9,21 +9,29 @@ public class OrderList {
 		int thirdNumber = 4;
 		int temp;
 		
-		// You can only use assignment operator "=" to assign 
-		// a number to another number.  For example:
-		// 
-		// firstNumber = secondNumber;
+		// Step 1 - Compare first 2 numbers.  If out of order, swap them.
+		if (firstNumber > secondNumber)  {
+			
+		}
+		
+		// Step 2 - Compare second and third numbers.  If out of order, swap them.
+		if (secondNumber > thirdNumber)  {
+			
+		}
+		
+		// Step 3 - Repeat Step 1.
+		if (secondNumber > thirdNumber)  {
+			
+		}
 		
 		System.out.println(firstNumber);
 		System.out.println(secondNumber);
 		System.out.println(thirdNumber);
 
 		// EXPECTED OUTPUT:
-		
 		// 3
 		// 4
 		// 9
-		
 	}
 
 }
