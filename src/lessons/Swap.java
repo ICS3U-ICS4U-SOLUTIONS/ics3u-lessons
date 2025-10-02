c95f53003f9f6007c0ff6acdad41a78853bfcece
e28166 <e28166@DESKTOP-56C86U1>
2025-10-02 08:15:57 -0400

    s

---
+++ src/lessons/Swap.java
@@ -0,0 +1,23 @@
+package lessons;
+
+public class Swap {
+
+	public static void main(String[] args) {
+
+		String cottonCanvas = "Picasso";
+		String linenCanvas = "Monet";
+
+		
+		// write any Java code
+
+		
+		System.out.println(cottonCanvas);
+		System.out.println(linenCanvas);
+
+		// Desired Output:
+		//
+		// Monet
+		// Picasso
+	}
+
+}
