c7bef7a04c81081d7eeede1d8d21745b2fbc4c23
e28166 <e28166@10.20.1.185>
2025-10-24 08:36:54 -0400

    s

---
+++ src/lessons/PredefinedMethodsStrings.java
@@ -0,0 +1,34 @@
+package lessons;
+import java.lang.String;  // this import is not necessary
+
+public class PredefinedMethodsStrings {
+
+	public static void main(String[] args) {
+
+		// predefined methods are "commands" which perform a specific task
+		// these methods were written by Java developers
+		// programmers, in turn, use these commands to perform complext tasks
+		// Methods are located in a file called a class, for example String.
+		
+		// variables
+		String myString = "Hello World!";		
+		String myString2 = " fubar";
+		
+		//System.out.println(myString.charAt(4));
+		
+		//String newString = "";
+		//newString = myString.concat(myString2);
+		//System.out.println(newString);
+				
+		// Display it
+		System.out.println(myString.length());
+		
+		// Use it
+		System.out.println(4 + myString.length());
+		
+		// Keep it
+		int numCars = myString.length();
+		
+	}
+
+}
