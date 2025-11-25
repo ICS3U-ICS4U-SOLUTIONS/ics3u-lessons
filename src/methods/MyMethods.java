8331081a4770f0ae7e43470d9f8feefbe73979e4
e28166 <e28166@DESKTOP-56C86U1>
2025-11-25 08:58:22 -0500

    s

--- src/methods/MyMethods.java
+++ src/methods/MyMethods.java
@@ -2,9 +2,41 @@ package methods;
 
 public class MyMethods {
 
-	// DESCRIPTION - Rounds double parameter to nearest hundredth.
-	// PARAMETERS - double a
-	// RETURN TYPE - double
+	public static void main()  {
+		
+		
+		System.out.println(factorial(-2));
+	}
+	
+	
+	/*	DESCRIPTION - Returns factorial of n>=0, else returns -1.
+	 * 	PARAMETERS - int n
+	 *  RETURN TYPE - int
+	 */
+	public static int factorial(int n)  {
+		
+		int answer = 1;
+				
+		// error case
+		if (n < 0)
+			return -1;
+		
+		// base case (n = 0)
+		if ((n == 0) || (n == 1))
+			return 1;
+		
+		// calculate factorial using loop
+		for (int i=1; i<=n; i++)
+			answer = answer * i;
+		
+		return answer;
+	}
+	
+	
+	/* DESCRIPTION - Rounds double parameter to nearest hundredth.
+	 * PARAMETERS - double a
+	 * RETURN TYPE - double
+	 */
 	public static double roundMoney(double a)  {
 		
 		a = a * 100;
