5d928b9cc024a436a87c502e8d77ae9f0dadf62b
e28166 <e28166@DESKTOP-56C86U1>
2025-12-15 08:28:31 -0500

    s

--- src/lessons/TimeProblem.java
+++ src/lessons/TimeProblem.java
@@ -7,32 +7,44 @@ public class TimeProblem {
 
 		// variables
 		Scanner sc = new Scanner(System.in);
-		int totalSeconds = 0;
-		int remainingSeconds = 0;
-		int seconds = 0;
-		int minutes = 0;
+		int input = 0;
 		int hours = 0;
+		int secondsRemaining = 0;
+		int minutes = 0;
+		int seconds = 0;
 		final int SECONDS_IN_HOUR = 3600;
-		final int SECONDS_IN_MINITUE = 60;
-
-		// get totalSeconds from user
-		System.out.println("Enter total number of second: ");
+		final int SECONDS_IN_MINUTE = 60;
 		
-		try {
-			
-			totalSeconds = sc.nextInt();
+		// get seconds from user
+		System.out.println("Enter number of seconds: ");
+		
+		try  {
+			input = sc.nextInt();
 		}
 		
-		catch(Exception fubar)  {
-			
-			fubar.printStackTrace();
+		catch(Exception e)  {
+			e.printStackTrace();
 		}
 		
 		// calculate number of hours
-		hours = totalSeconds / SECONDS_IN_HOUR;
+		hours = input / SECONDS_IN_HOUR;
+		
+		// calculate seconds remaining
+		secondsRemaining = input % SECONDS_IN_HOUR;
+		
+		// calculate minutes
+		minutes = secondsRemaining / SECONDS_IN_MINUTE;
+		
+		// calculate seconds
+		seconds = secondsRemaining % SECONDS_IN_MINUTE;
 		
 		
-		
+		// output
+		System.out.println("Seconds entered: " + input);
+		System.out.println("HOURS: " + hours);
+		System.out.println("SECONDS REMAINING AFTER HOURS CALCULATED: " + secondsRemaining);
+		System.out.println("MINUTES: " + minutes);
+		System.out.println("SECONDS: " + seconds); 
 	
 	}
 
