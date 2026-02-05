8a74262fbf7883a6fd71ec87a7af6b2e28906583
e28166 <e28166@FXH-9496082790>
2026-02-05 12:01:01 -0500

    s

---
+++ src/lessons/DivisionPractice.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class DivisionPractice {
+
+	public static void main(String[] args) {
+
+
+		int answer = 5/3;
+		
+		System.out.println(answer);
+
+	}
+
+}
---
+++ src/lessons/MathOperators.java
@@ -0,0 +1,40 @@
+package lessons;
+
+public class MathOperators {
+
+	public static void main(String[] args) {
+		
+		// Math operators
+		// +, -, *, /, %
+		
+		// variables
+		int myInteger1 = 13;
+		int sum = 0;
+		int difference = 0;
+		int product = 0;
+		double quotient = 0;
+		
+		// addition
+		sum = myInteger1 + 4;
+		System.out.println(sum);
+
+		// subtraction
+		difference = 13 - 5;
+		System.out.println(difference);
+		
+		// multiplication
+		product = 3 * 5;
+		System.out.println(product);
+		
+		/*
+		 * DIVISION: please understand all the possible variations of dividing int and doubles
+		 * 
+		 */
+		
+		
+		quotient = 5.0/2.0;
+		System.out.println(quotient);
+		
+	}
+
+}
