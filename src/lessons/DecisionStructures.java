521e2754bc14060756a5906d80f3a3b9077977b1
e28166 <e28166@FXH-9496082790>
2026-02-06 08:57:46 -0500

    sdf

--- src/lessons/DecisionStructures.java
+++ src/lessons/DecisionStructures.java
@@ -4,7 +4,19 @@ public class DecisionStructures {
 
 	public static void main(String[] args) {
 
+		/*
+		 *  Comparison operators: >, >=, <, <=, ==, !=
+		 */
 		
+		if ( 13 != 13)  {
+			
+			System.out.println("true");
+		}
+		
+		else  {
+			
+			System.out.println("false");
+		}
 
 	}
 
