e029e14e80a1ceef4eeb32a48714b7243c573567
e28166 <e28166@FXH-9496082790>
2026-02-19 09:07:34 -0500

    sfd

--- src/lessons/TryCatchGUI.java
+++ src/lessons/TryCatchGUI.java
@@ -1,13 +1,21 @@
 package lessons;
 
+import javax.swing.JOptionPane;		// import for error dialog
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TryCatchGUI {
 
+	// variables
 	private JFrame frame;
-
+	private JTextField textField;
+	int myInteger = 0;				// variable will be visible throughout program
+	
 	/**
 	 * Launch the application.
 	 */
@@ -39,6 +47,30 @@ public class TryCatchGUI {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(151, 99, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Enter int");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// button code start
+				try {
+					myInteger = Integer.parseInt(textField.getText());
+				}
+				
+				catch(Exception e2)  {
+					
+					JOptionPane.showMessageDialog(null, "Invalid.");
+				}
+				
+				// button code end
+			}
+		});
+		btnNewButton.setBounds(151, 154, 89, 23);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
