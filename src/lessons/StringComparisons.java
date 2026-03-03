391150c00746a4f979e258f49886fbb6dfc0a8a7
e28166 <e28166@FXH-9496082790>
2026-03-03 08:56:58 -0500

    asdf

---
+++ src/lessons/StringComparisons.java
@@ -0,0 +1,25 @@
+package lessons;
+
+public class StringComparisons {
+
+	public static void main(String[] args) {
+
+		// variables
+		String word1 = "foo";
+		String word2 = "bar";
+		String word3 = "foo";
+		
+		if (word1.equals(word2))  {
+			
+			System.out.println("Same words.");
+		}
+		
+		else  {
+			
+			System.out.println("Different words.");
+		}
+		
+
+	}
+
+}
