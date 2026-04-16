c6912c7f9ed62a0c56d0cc26ad94d2c14c7e27f1
e28166 <e28166@FXH-9496082790>
2026-04-16 08:54:15 -0400

    asdf

---
+++ src/lessons/UserDefinedMethods.java
@@ -0,0 +1,13 @@
+package lessons;
+import java.lang.Math;
+import methods.MyMethods;
+
+
+public class UserDefinedMethods {
+
+	public static void main(String[] args) {
+		
+		MyMethods.printMenu();
+		System.out.println(MyMethods.biggerOfTwoNumbers(1, 3));
+	}
+}
---
+++ src/methods/MyMethods.java
@@ -0,0 +1,35 @@
+package methods;
+
+public class MyMethods {
+
+	
+	/*
+	 * 	DESCRIPTION - Displays random program menu.
+	 * 	PARAMETERS - none
+	 * 	RETURN TYPE - void
+	 */
+	public static void printMenu()  {
+		
+		System.out.println("Welcome to my program.\n");
+		System.out.println("1. Do something.");
+		System.out.println("2. Do something else.");
+		System.out.println("3. Do nothing.");
+		System.out.println("4. Exit.");
+		System.out.println("Your choice: ");
+	}
+	
+	
+	/*
+	 * 	DESCRIPTION - Returns greater of x and y.
+	 * 	PARAMETERS - int x, int y
+	 * 	RETURN TYPE - int
+	 */
+	public static int biggerOfTwoNumbers(int x, int y)  {
+		
+		if (x > y)  
+			return x;
+			
+		return y;		
+	}
+
+}
