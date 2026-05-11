edb21484ee0b11089f81c0ebd9a455abfee18fca
e28166 <e28166@FXH-9496082790>
2026-05-11 09:18:27 -0400

    sadf

--- src/lessons/PseudoCode.java
+++ src/lessons/PseudoCode.java
@@ -48,25 +48,91 @@ public class PseudoCode {
 		// variables
 		Scanner sc = new Scanner(System.in);
 		int totalSeconds = 0, remainingSeconds = 0, hours = 0, minutes = 0, seconds = 0;
-
+		final int SECONDS_IN_HOUR = 3600;
+		final int SECONDS_IN_MINUTE = 60;
+		
 	 	// Get the number of seconds from the user.
 		totalSeconds = getNumberOfSeconds(sc);
 		
+		// Calculate the number of hours
+		hours = calculateNumberHours(totalSeconds, SECONDS_IN_HOUR);
 		
-		/*
-		2. Calculate the number of hours.
-	 	3. Calculate the number of minutes.
-	 	4. Calculate the number of seconds.
-	 	5. Output the number of hours, minutes and seconds.
-		*/
+		// Calculate remaining seconds
+		remainingSeconds = calculateRemainingSeconds(totalSeconds, SECONDS_IN_HOUR);
+		
+		// Calculate the number of minutes
+		minutes = calculateNumberMinutes(remainingSeconds, SECONDS_IN_MINUTE);
+		
+		// Calculate the number of seconds
+		seconds = calculateNumberSeconds(remainingSeconds, SECONDS_IN_MINUTE);
+		
+		// Display hours, minutes and seconds
+		displayTimes(hours, minutes, seconds);
+		
 		
 	}
 	
-	/*	DESCRIPTION - Gets number seconds from user.
-	 * 	PARAMTERS - Scanner sc
+	
+	/*	DESCRIPTION - Displays hours, minutes and seconds
+	 * 	PARAMTERS - int h, int m, int s
+	 * 	RETURN - void
+	 */	
+	public static void displayTimes(int h, int m, int s)  {
+		
+		System.out.println("HOURS: " + h);
+		System.out.println("MINUTES: " + m);
+		System.out.println("SECONDS: " + s);
+	}
+	
+	
+	/*	DESCRIPTION - Calculates number of seconds using remaining seconds.
+	 * 	PARAMTERS - int remainSecs, int secsInMin
 	 * 	RETURN - int
 	 */	
-	public static int getNumberOfSeconds(Scanner sc)  {
+	public static int calculateNumberSeconds(int remainSecs, int secsInMin)  {
+		
+		return(remainSecs%secsInMin);
+	}
+	
+	
+	/*	DESCRIPTION - Calculates number of minutes using remaining seconds after calculating number hours.
+	 * 	PARAMTERS - int remainSecs, int secsInMin
+	 * 	RETURN - int
+	 */	
+	public static int calculateNumberMinutes(int remainSecs, int secsInMin)  {
+		
+		return(remainSecs/secsInMin);
+	}
+	
+	
+	/*	DESCRIPTION - Calculates remaining seconds after calculating total number hours.
+	 * 	PARAMTERS - int s, int secsInHour
+	 * 	RETURN - int
+	 */	
+	public static int calculateRemainingSeconds(int s, int secsInHour)  {
+		
+		return(s%secsInHour);
+	}
+	
+	
+	/*	DESCRIPTION - Calculates number of hours using total number of seconds.
+	 * 	PARAMTERS - int s, int secsInHour
+	 * 	RETURN - int
+	 */	
+	public static int calculateNumberHours(int s, int secsInHour)  {
+		
+		return(s/secsInHour);
+	}
+	
+	
+	
+	
+	
+	/*	DESCRIPTION - Gets number seconds from user.
+	 * 	PARAMTERS - Scanner s
+	 * 	RETURN - int
+	 */	
+	public static int getNumberOfSeconds(Scanner s)  {
 		
 		int seconds = 0;
 		
@@ -74,7 +140,7 @@ public class PseudoCode {
 		
 		try  {
 			
-			seconds = sc.nextInt();
+			seconds = s.nextInt();
 		}
 		
 		catch(Exception e)  {
