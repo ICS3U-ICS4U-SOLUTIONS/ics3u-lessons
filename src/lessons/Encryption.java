6e431b85690e132b3247cba5af27d97b7908020a
e28166 <e28166@FXH-9496082790>
2026-05-28 08:50:16 -0400

    asfd

--- src/lessons/Encryption.java
+++ src/lessons/Encryption.java
@@ -4,7 +4,16 @@ public class Encryption {
 
 	public static void main(String[] args) {
 		
+		int shift = 3;
+		
+		// cast to int
+		int ascii = (int)'a';
+		System.out.println(ascii);
 
+		ascii = ascii + shift;
+		System.out.println((char)ascii);
+		
+		
 	}
 
 }
