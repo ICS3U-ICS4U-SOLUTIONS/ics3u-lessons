31d9e858a4de934e67b5198ee46b6c8354b06970
e28166 <e28166@FXH-9496082790>
2026-09-09 09:06:43 -0400

    asdf

---
+++ src/lessons/Variables.java
@@ -0,0 +1,23 @@
+package lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		// variables
+		int myInteger = 0;		// initialize variables
+		double myDouble = 13.3;	// decimal values
+		char myChar = 'x';		// any character on keyboard, use single quotes
+		boolean myBoolean = true;	// can be true or false
+		String myString = "fubar foo bar";	// anything between double quotes is part of String
+		
+		// CONSTANT_VARIABLES
+		// Variables which should not change.
+		// Values can not change within program, but changeable upon declaration.
+		// Use keyword "final" to make it constant.
+		// Use all capital letters with underscore between words by convention.
+		final double TAX_RATE = 13.0;
+
+	}
+
+}
