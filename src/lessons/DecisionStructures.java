5a770b96573163a5f31e5cc2cc129469309fb16a
e28166 <e28166@FXH-9496082790>
2026-09-10 09:02:58 -0400

    asdf

--- src/lessons/DecisionStructures.java
+++ src/lessons/DecisionStructures.java
@@ -4,8 +4,27 @@ public class DecisionStructures {
 
 	public static void main(String[] args) {
 
+		// Conditions are comparisons, they will evalutate to a 
+		// boolean value (true or false).  If the overall statement
+		// is true, the code will run.  Comparisons use operators to
+		// compare numbers or words.
+		
+		// >, <, >=, <=, ==, !=
+		
+		// Be sure to understand all the comparison operators.
+		// Change the numbers and the operators and anticipate
+		// what the expected outcome should be before executing
+		// code.
+		
+		if (1 >= 1) {
+			
+			System.out.println("True");
+		}
 
-
+		else  {
+			
+			System.out.println("False");
+		}
 	}
 
 }
